0fd9d6f043c4ea3a532a7681d0ec72c8b0e2f671
Victor Valente <Valente-victor@hotmail.com>
2023-08-30 17:13:36 -0300

    Iniciando rotas na aplicacao

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  link,
+  useParams,
+} from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/:nomeCurso/:nomeModulo/:nomeAula">
+            <div className="aulas-video">Mostrar Aula</div>
+          </Route>
+          <Route path="/:nomeCurso/:nomeModulo">
+            <div className="aulas">Listar Aulas</div>
+          </Route>
+          <Route path="/:nomeCurso">
+            <div className="modulos">Listar Módulos</div>
+          </Route>
+          <Route path="/">
+            <div className="cursos">Listar Cursos</div>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
